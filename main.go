75bc529620baf222ba565d24e6209680097f75cd
Aadi Deshmukh <aadi14des@gmail.com>
2026-07-22 17:24:05 +0530

    Added postgres config and starting api authentication

--- main.go
+++ main.go
@@ -1,23 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/aadides/rssaggre/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+type apiConfig struct{
+	DB *database.Queries
+}
 
 func main() {
 	fmt.Println("Lets begin")
 	godotenv.Load()
-	portString := os.Getenv("PORT")
 
+	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the env")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == ""{
+		log.Fatal("DB_URL is not in the env")
+	}
+
+	conn, err := sql.Open("postgres",dbURL)
+	if err!= nil{
+		log.Fatal("Cant connect to database:", err)
+	}
+	
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 	router := chi.NewRouter()
 
@@ -33,6 +54,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness) //healthz is kubernetes convention
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerUsersCreate)
 
 	router.Mount("/v1", v1Router)
 
@@ -42,7 +64,7 @@ func main() {
 	}
 	log.Printf("Server starting on port %v", portString)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
